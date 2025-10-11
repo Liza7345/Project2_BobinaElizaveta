e4243f75472e24841fb6d51674b294646699072c
BobinaLiza <bobinaliza14@gmail.com>
2025-10-11 10:05:09 +0300

    Project2_Bobina

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,35 @@ fun countdigits (arr: Array<IntArray>): Int {
     return digitset.size
 }
 fun main(args: Array<String>) {
-    println ("ведите строки: ")
+    println ("ведите количество строк: ")
     val rows = readLine()?.toIntOrNull()?:
     return println ("Error")
-
+    println ("ведите количество столбцов: ")
+    val cols = readLine()?.toIntOrNull()?:
+    return println("Error")
+    val arr = Array (rows){IntArray(cols)}
+    println("Введите числа: ")
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            while (true) {
+                val v = readLine()
+                val num = v?.toIntOrNull()
+                if (num != null && num in 100..999) {
+                    arr[i][j] = num
+                    break
+                }
+                else {
+                    println("Error")
+                }
+            }
+        }
+    }
+    for (row in arr) {
+        for (num in row) {
+            print ("$num\t")
+        }
+        println()
+    }
+    val a = countdigits (arr)
+    println("В массиве используются $a различных символов.")
 }
